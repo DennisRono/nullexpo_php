19856c17c6cee0a3c2c094f922ae855060296e38
Finn Neron <72130813+DennisRono@users.noreply.github.com>
2021-06-19 10:07:06 +0300

    Update mail_verification.php
    Edit

--- member/sendmail/verification/mail_verification.php
+++ member/sendmail/verification/mail_verification.php
@@ -9,8 +9,8 @@ $_POST['email'] = $Email;
 $link = 'href="localhost/nullexpo/member/verify.php?token='.$token.'"';
 $mail = new Email('smtp.gmail.com', 587);
 $mail->setProtocol(Email::TLS)
-    ->setLogin('finnneron1@gmail.com', '@neron27finn')
-    ->setFrom('finnneron1@gmail.com')
+    ->setLogin('Email address here', 'passwordHere')
+    ->setFrom('Email address here')
     ->setSubject('Welcome to NullExpo')
     ->setTextMessage('Plain text message')
     ->setHtmlMessage('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>NullExpo</title><style>body{background-color:rgb(225,225,225);margin:0px;padding:0px;height:auto}.container{background-color:rgb(118,26,170);height:50vh;color:#fff;text-align:center}.container>h2{padding-top:40px;margin-bottom:30px}.button{background-color:rgb(26, 179, 176);padding:10px;border-radius:5px;margin-top:50px;color:#fff;text-decoration:none}</style></head><body><div class="container"><h2>Welcome to NullExpo</h2><p>Welcome to the evergrowing marketing giant where you will make extra cash!</p> <br><br> <a class="button" '.$link.'> verify your account </a></div></body></html>')
